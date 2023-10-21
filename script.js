ecdcfd2dc59654053b7b5eaf73182dc93bcb0154
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-21 15:50:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(key =='blue')
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+	
+	
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+//const key = prompt("Enter Key.");
+//alert(hasKey(key));
